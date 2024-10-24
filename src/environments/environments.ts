85ee50756408cb01dbdf506c4c4f5af6a15f2f83
inyat.nathani <inyat.nathani@gmail.com>
2024-10-24 10:07:31 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyAVU23TxBBcPwBqkFT8g0lJKZlshyGuPfM',
+      authDomain: 'in-firebase-ai-app.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'in-firebase-ai-app',
+      storageBucket: 'in-firebase-ai-app.appspot.com',
+      messagingSenderId: '270013249226',
+      appId: '1:270013249226:web:793b6604b537261d27e633',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyDsOjVHfX7sRP2OQTbMOeqjq0fj1jRvt2E',
+  debug_token: ''
+};
